f5ca8e14bf64f615b2e810b5e76a028bcf38df83
Illia.Hrynyshyn <Illia.Hrynyshyn@nzsu.gov.com>
2024-09-11 14:54:50 +0300

    implement del & list expenses

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -2,8 +2,15 @@
 
 from .start import start_router
 from .add_expenses import expenses_router
+from .list_expenses import list_expenses_router
+from .delete_expenses import delete_expense_router
 
-routers_list = [start_router, expenses_router]
+routers_list = [
+    start_router,
+    list_expenses_router,
+    delete_expense_router,
+    expenses_router,
+]
 
 __all__ = [
     "routers_list",
---
+++ tgbot/handlers/delete_expenses.py
@@ -0,0 +1,25 @@
+import re
+from dataclasses import dataclass
+from aiogram import types, Router
+from aiogram.filters import Filter
+from aiogram import F
+
+from infrastructure.database.db import DataBase
+from tgbot.handlers.categories import Categories, Category
+from tgbot.handlers.errors import error_handler
+from tgbot.handlers.errors.error_handler import NotCorrectMassage
+# from tgbot.handlers.errors.error_handler import NotCorrectMassage
+from tgbot.misc.datetime_now import _get_now_formatted
+
+db = DataBase()
+expenses_router = Router()
+expenses_router.message()
+
+delete_expense_router = Router()
+
+
+@delete_expense_router.message(F.text.regexp("^/del"))
+async def del_expense(message: types.Message):
+    row_id = int(message.text[4:])
+    db.delete("expense", row_id)
+    await message.answer(f"Запис №{row_id} удалена")
